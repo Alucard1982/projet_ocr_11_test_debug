75603441447c11ffe178b5d46a79c47ab37d7ac2
Florent <florentpeyre@hotmail.fr>
2021-02-15 15:48:41 +0100

    all bug finish

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ def purchasePlaces():
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         club['points'] = int(club['points']) - placesRequired
         flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions, datetime=datetime)
 
 
 # TODO: Add route for points display
